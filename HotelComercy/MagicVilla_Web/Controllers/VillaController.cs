a25bfea66c9a2bd0147e671cfe7d8a8566ff7762
Lucas Reis <luke.spqr@gmail.com>
2022-11-29 10:17:45 -0300

    Adding Villa Controller and Index Action

---
+++ HotelComercy/MagicVilla_Web/Controllers/VillaController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace MagicVilla_Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly IVillaService _service;
+        private readonly IMapper _mapper;
+
+        public VillaController(IVillaService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> IndexVilla()
+        {
+            List<VillaDTO> villa = new List<VillaDTO>();
+            var response = await _service.GetAllAsync<ApiResponse>();
+            if(response != null && response.IsSucess)
+            {
+                villa = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result));
+            }
+            return View(villa);
+        }
+    }
+}
